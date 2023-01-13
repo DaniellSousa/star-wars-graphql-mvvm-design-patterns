ab13aff2b367d897cbdb3789238a7d700bb9bf9b
daniel_monteiro_ <daniell.monteirosousa@gmail.com>
2023-01-13 18:30:48 -0300

    Upgrade libraries; modules for core and network; test request apollo client

--- app/src/main/java/com/daniellms/star_wars_graphql_mvvm_design_patterns/MainActivity.kt
+++ app/src/main/java/com/daniellms/star_wars_graphql_mvvm_design_patterns/MainActivity.kt
@@ -2,10 +2,29 @@ package com.daniellms.star_wars_graphql_mvvm_design_patterns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.apollographql.apollo3.ApolloClient
+import com.daniellms.star_wars_graphql_mvvm_design_patterns.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val apolloClient = ApolloClient.Builder()
+//            .serverUrl("https://e72b-2804-7f7-e289-c46d-9071-342f-19b1-3ef7.sa.ngrok.io/")
+//            .build()
+//
+//        runBlocking {
+//            withContext(Dispatchers.IO) {
+//                val response = apolloClient.query(GetFilmsQuery()).execute()
+//                Log.i("RESULT GRAPHQL", "${response.data?.allFilms?.films}")
+//            }
+//        }
     }
 }
